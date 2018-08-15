0f7cd2ab7295de7012cf7a9a9a3d2751c915cbaa
Rasmus Høhndorf Hummelmose <rasmus@hummelmose.dk>
2018-08-15 19:58:58 +0200

    Testing

--- TabbedPageWithNavigationPage/TabbedPageWithNavigationPage/MainPage.xaml.cs
+++ TabbedPageWithNavigationPage/TabbedPageWithNavigationPage/MainPage.xaml.cs
@@ -7,7 +7,22 @@ namespace TabbedPageWithNavigationPage
 		public MainPage ()
 		{
 			InitializeComponent ();
-		}
+
+            var commonPageIcon = "schedule.png";
+
+            var scheduleNavigationPage = new NavigationPage(new SchedulePage());
+            scheduleNavigationPage.Icon = commonPageIcon;
+            scheduleNavigationPage.Title = "Schedule";
+
+            var countriesNavigationPage = new NavigationPage(new CountriesPage());
+            countriesNavigationPage.Icon = commonPageIcon;
+            countriesNavigationPage.Title = "Countries";
+
+            Children.Add(new TodayPage());
+            Children.Add(scheduleNavigationPage);
+            Children.Add(new SettingsPage());
+            Children.Add(countriesNavigationPage);
+        }
 	}
 }
 
